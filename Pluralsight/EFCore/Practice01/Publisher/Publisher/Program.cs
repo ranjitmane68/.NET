ed5b3720b505efaf97003be42052345cdacaf404
Ranjit Mane <ranjitmane68@gmail.com>
2024-10-03 09:44:19 +0530

    Implemented EF Core starter project

---
+++ Pluralsight/EFCore/Practice01/Publisher/Publisher/Program.cs
@@ -0,0 +1,30 @@
+﻿using PublisherData;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Main is called");
+        using (PubContext context = new PubContext())
+        {
+            context.Database.EnsureCreated();
+        }
+
+        GetAuthors();
+
+    }
+
+    static void GetAuthors()
+    {
+        Console.WriteLine("Getting Authrors");
+        using var context = new PubContext();
+        var authors = context.Authors.ToList();
+
+        foreach (var auth in authors)
+        {
+            Console.WriteLine(auth.Name);
+        }
+    }
+}
+
